5afdd204fb6385b9847a5f419ae13e904b18d20e
Mehdi <maehdiaitmousse70@gmail.com>
2025-09-18 17:50:39 +0100

    brand schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,7 +1,8 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { categoryType } from './categoryType'
 import { addressType } from './addressType'
+import { brandType } from './brandType'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [categoryType,addressType],
+  types: [categoryType, addressType, brandType],
 }
