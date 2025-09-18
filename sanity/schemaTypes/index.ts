486a2b1dec1094c787ff91c0cef8d6337fdb96ae
Mehdi <maehdiaitmousse70@gmail.com>
2025-09-18 18:35:28 +0100

    Blog schema

--- sanity/schemaTypes/index.ts
+++ sanity/schemaTypes/index.ts
@@ -1,8 +1,24 @@
-import { type SchemaTypeDefinition } from 'sanity'
-import { categoryType } from './categoryType'
-import { addressType } from './addressType'
-import { brandType } from './brandType'
+import { type SchemaTypeDefinition } from "sanity";
+import { categoryType } from "./categoryType";
+import { blockContentType } from "./blockContentType";
+import { productType } from "./productType";
+import { orderType } from "./orderType";
+import { blogType } from "./blogType";
+import { blogCategoryType } from "./blogCategoryType";
+import { authorType } from "./authorType";
+import { addressType } from "./addressType";
+import { brandType } from "./brandType";
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [categoryType, addressType, brandType],
-}
+  types: [
+    categoryType,
+    blockContentType,
+    productType,
+    orderType,
+    brandType,
+    blogType,
+    blogCategoryType,
+    authorType,
+    addressType,
+  ],
+};
